ca0315fa90a61912fc884643069e303eb4fe8b92
Joseph Brian <natividadjosephbrian@gmail.com>
2025-09-26 21:47:39 +0800

    Learn require module.exports

---
+++ 4-names.js
@@ -0,0 +1,8 @@
+// local
+const secret = "SUPER SECRET";
+// share
+const joseph = "joseph";
+const brian = "brian";
+
+module.exports = { joseph, brian };
+
--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-// Modules
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated code (only share minimum)
+const names = require(`./4-names`);
+const sayHi = require(`./5-utils`);
+// console.log(names);
 
+sayHi(`iya`);
+sayHi(names.joseph);
+sayHi(names.brian);
